c24927ca38c1927e6e5b2b250042c068b4db59c9
wolfram <sevenup96@mail.ru>
2017-04-18 14:25:11 +0300

    CheckGitInIdea

--- src/MainArchive.java
+++ src/MainArchive.java
@@ -5,6 +5,8 @@ public class MainArchive {
     public static void main(String[] args) {
         String[] s = {"/home/wolfram/lol.txt", "/home/wolfram/lol1.txt", "-o", "/asdasd", "-q", "123"};
         NoGuiModel noGuiModel = new NoGuiModel(s);
+
+
 //        noGuiModel.out();
 
 //        if (args[0].equals("--no-gui")) {
